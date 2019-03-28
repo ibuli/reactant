a4180b54cea47b6807efeaa6743eb8ee44e2af29
Burhan <bharmal.burhan@hotmail.com>
2019-03-28 18:51:51 +0530

    remove old code from creat-react-app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <div>
+    <h1>Hello to Reactant</h1>
+  </div>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
