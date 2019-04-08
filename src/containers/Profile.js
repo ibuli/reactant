d10435d3f3ed1757d2066951b5a4617e592ea682
Burhan <bharmal.burhan@hotmail.com>
2019-04-08 16:40:45 +0530

    - add Konva and react-konva library to draw tic-tac-toe squares - rename Home.js to TicTacToe.js - add styled components - update Template JS with logic of TicTacToe - add media.js to write media queries for responsive behaviours

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react'
+import {
+  Container,
+  Name,
+  GameListHeader,
+  GameList,
+  GameRecord,
+  Column,
+  ColumnLabels,
+} from '../styled/Profile'
 
 class Profile extends Component {
+  static defaultProps = {
+    user: {
+      email: 'USER_EMAIL',
+      games: [
+        {
+          winner: true,
+          createdAt: '12/25/2019',
+          id: '0001',
+        },
+        {
+          winner: false,
+          createdAt: '12/26/2019',
+          id: '0002',
+        },
+        {
+          winner: true,
+          createdAt: '12/27/2019',
+          id: '0003',
+        },
+      ],
+    },
+  }
+
+  get records() {
+    return this.props.user.games.map((game, index) => {
+      return (
+        <GameRecord key={index} index={index}>
+          <Column>{game.winner ? 'Won!' : "Didn't Win!"}</Column>
+          <Column>"Robot"</Column>
+          <Column>"No"</Column>
+          <Column>{game.createdAt}</Column>
+        </GameRecord>
+      )
+    })
+  }
   render() {
+    let { email } = this.props.user
     return (
-      <div>
-        <h2>Profile</h2>
-      </div>
+      <Container>
+        <Name>{email}</Name>
+        <GameList>
+          <GameListHeader>My Games</GameListHeader>
+          <ColumnLabels>
+            <Column>Outcomme</Column>
+            <Column>Guess</Column>
+            <Column>Guessed Correctly</Column>
+            <Column>Date</Column>
+          </ColumnLabels>
+          {this.records}
+        </GameList>
+      </Container>
     )
   }
 }
--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import NavDrawer from '../components/NavDrawer'
+import { Header, Main } from '../styled/Template'
 class Template extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>
-          <NavDrawer />
-          <main>{this.props.children}</main>
-        </div>
+        <NavDrawer />
+        <Header>Reactant</Header>
+        <Main>{this.props.children}</Main>
       </MuiThemeProvider>
     )
   }
